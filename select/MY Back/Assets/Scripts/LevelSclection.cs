56fd6dfa4764758f09de790f7d33dc1bad0bf655
Fortunate4551 <stu1904551@cgt.bitzh.edu.cn>
2022-05-09 14:54:41 +0800

    选关

---
+++ select/MY Back/Assets/Scripts/LevelSclection.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSclection : MonoBehaviour
+{
+    [SerializeField] private bool unlocked;//Default value is false；
+    public Image unlockImage;
+    public GameObject[] starts;
+
+    private void Update()
+    {
+        UpdateLevelImage();
+        
+    }
+
+    private void UpdateLevelImage()
+    {
+        if (!unlocked)//if unloocked is false ,means it is clocked;
+        {
+            unlockImage.gameObject.SetActive(true);
+            for (int i = 0; i < starts.Length; i++)
+            {
+                starts[i].gameObject.SetActive(false);
+            }
+        }
+        else//if unlock is ture means the level can play;
+        {
+            unlockImage.gameObject.SetActive(false);
+            for (int i = 0; i < starts.Length; i++)
+            {
+                starts[i].gameObject.SetActive(true);
+            }
+        }
+    }
+    public void PressSelection(string _levelName)//when you press this level ,we can move to this scene;
+    {
+        if (unlocked)
+        SceneManager.LoadScene(_levelName);
+    }
+
+
+
+}
